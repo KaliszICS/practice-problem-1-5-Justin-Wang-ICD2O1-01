8efefa4336b51bc35272a8e6d3f723c88e882b1b
Justin-Wang-ICD2O1-01 <Justin.wang24@student.tdsb.on.ca>
2024-11-07 10:03:52 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -1,43 +1,40 @@
 
 import math
 
-
 def q1():
   #Write Assignment code here
-    One = input("Input an integer: ")
-    One = int(One)
-    One = One + 3 
-    print("The integer is: ", One)
+    word1 = input("Input an integer: ")
+    word2 = int(word1)
+    word3 = word2 + 3 
+    print("The integer is: ", word3)
 def q2():
   #Write Assignment code here
-   One = input("Input a number: ")
-   One = One + "4"
-   One = (float(One))
-   One = One + 2
-   print("A number is: ", One)
+    word4 = input("Input a number: ")
+    word5 = word4 + "4"
+    word6 = (float(word5))
+    word7 = word6 + 2
+    print("A number is: ", word7)
 def q3():
   #Write Assignment code here
-   Num = input("Input a radius: ")
-   radius = (float(Num))
-   Pi = 3.14
-   area = Pi * (radius ** 2)
-   print("The area of a circle is: ", area)
+    word8 = input("Input a radius: ")
+    radius = (float(word8))
+    pi = 3.14
+    area = pi * (radius ** 2)
+    print("The area of a circle is: ", area)
 def q4():
   #Write Assignment code here
-  Num = input("Input a number: ")
-  Num = (float(Num))
-  Num = Num * 12 
-  Num = math.floor(Num)
-  print("The number is: ", Num)
+    word9 = input("Input a number: ")
+    word10 = (float(word9))
+    word11 = word10 * 12 
+    word12 = math.floor(word11)
+    print("The number is: ", word12)
 def q5():
   #Write Assignment code here
-  Set = input("Input an integer: ")
-  Set = (int(Set))
-  Set = Set+5
-  print("Your number is : ", Set)
-#Comment this code out when running tests
-#Do not comment this out when running your program normally
-
+    set1 = input("Input an integer: ")
+    set2 = (int(set1))
+    set3 = set2 + 5
+    print("Your number is: ", set3)
+    
 #q1()
 #q2()
 #q3()
