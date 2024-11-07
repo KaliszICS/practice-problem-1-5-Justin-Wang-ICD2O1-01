61790e3e382c9d37485a5eca0f792f03a695101c
Justin-Wang-ICD2O1-01 <Justin.wang24@student.tdsb.on.ca>
2024-11-07 10:08:59 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -6,34 +6,34 @@ def q1():
     word1 = input("Input an integer: ")
     word2 = int(word1)
     word3 = word2 + 3 
-    print("The integer is: ", word3)
+    print(word3)
 def q2():
   #Write Assignment code here
     word4 = input("Input a number: ")
     word5 = word4 + "4"
     word6 = (float(word5))
     word7 = word6 + 2
-    print("A number is: ", word7)
+    print(word7)
 def q3():
   #Write Assignment code here
     word8 = input("Input a radius: ")
     radius = (float(word8))
     pi = 3.14
     area = pi * (radius ** 2)
-    print("The area of a circle is: ", area)
+    print(area)
 def q4():
   #Write Assignment code here
     word9 = input("Input a number: ")
     word10 = (float(word9))
     word11 = word10 * 12 
     word12 = math.floor(word11)
-    print("The number is: ", word12)
+    print(word12)
 def q5():
   #Write Assignment code here
     set1 = input("Input an integer: ")
     set2 = (int(set1))
     set3 = set2 + 5
-    print("Your number is: ", set3)
+    print(set3)
     
 #q1()
 #q2()
